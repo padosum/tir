ce7f1d090bf3a134d6d7a37665f18f3454209ef0
padosum <makalu171@gmail.com>
2021-08-03 23:19:35 +0900

    💄 add side navbar styles

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <nav-bar></nav-bar>
-    <router-view></router-view>
+    <div class="wrapper">
+      <nav-bar></nav-bar>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
-
 export default {
-  components: { NavBar },
+  components: {
+    NavBar,
+  },
   name: 'app',
   data() {
     return {}
+  },
+  computed: {
+    categories() {
+      let arr = []
+      this.data
+      return arr
+    },
   },
   mounted() {},
 }
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&display=swap');
-@import '~assets/style/reset.css';
-@import '~assets/style/common.css';
-
-#app {
-  font-family: 'Noto Sans KR', sans-serif;
-}
+<style lang>
+@import url('~assets/style/base.scss');
+@import url('~assets/style/main.scss');
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,131 @@
 <template>
-  <nav>
-    <router-link :to="logoLink" class="">📖 TIR</router-link>
-  </nav>
+  <section class="navbar">
+    <div class="menu-container">
+      <div>
+        <nav class="navbar-links">
+          <div class="categories">
+            <li :class="selected('Art')">
+              <router-link :to="'/Art'">
+                <i class="fas fa-palette"></i> Art
+              </router-link>
+            </li>
+            <li :class="selected('Business')">
+              <router-link :to="'/Business'"
+                ><i class="fas fa-briefcase"></i> Business</router-link
+              >
+            </li>
+            <li :class="selected('Data-Science')">
+              <router-link :to="'/Data-Science'"
+                ><i class="fas fa-database"></i> Data Science</router-link
+              >
+            </li>
+            <li :class="selected('Design')">
+              <router-link :to="'/Design'"
+                ><i class="far fa-object-ungroup"></i> Design</router-link
+              >
+            </li>
+            <li :class="selected('Dev')">
+              <router-link :to="'/Dev'"
+                ><i class="fas fa-terminal"></i> Dev</router-link
+              >
+            </li>
+            <li :class="selected('History')">
+              <router-link :to="'/History'"
+                ><i class="fas fa-landmark"></i> History</router-link
+              >
+            </li>
+            <li :class="selected('Life')">
+              <router-link :to="'/Life'"
+                ><i class="fas fa-life-ring"></i> Life</router-link
+              >
+            </li>
+            <li :class="selected('Study')">
+              <router-link :to="'/Study'"
+                ><i class="fas fa-pencil-alt"></i> Study</router-link
+              >
+            </li>
+            <li :class="selected('Technology')">
+              <router-link :to="'/Technology'"
+                ><i class="fas fa-microchip"></i> Technology</router-link
+              >
+            </li>
+            <li :class="selected('Test')">
+              <router-link :to="'/Test'"
+                ><i class="fas fa-vial"></i> Test</router-link
+              >
+            </li>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  computed: {
-    logoLink() {
-      return '/'
+  methods: {
+    selected(name) {
+      return this.$route.params.category === name ? 'selected' : ''
     },
   },
 }
 </script>
 
-<style scoped>
-nav {
-  background-color: #1867c0;
-  color: #fff;
-  padding: 10px 10px 10px 30px;
+<style lang="scss" scoped>
+$background-grey: #efefef;
+$text-light-grey: #9f9f9f;
+$background-hover-grey: #e4e4e4;
+
+.navbar {
+  position: relative;
+  background: #323246;
+  width: 250px;
+  flex-shrink: 0;
+}
+
+.navbar-links {
+  height: calc(100vh - 120px);
+  font-size: 13px;
+  font-weight: 700;
+}
+
+.menu-container {
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  padding-left: 10px;
+  padding-right: 20px;
+  padding-top: 18px;
+  font-weight: 300;
 }
-a {
-  color: white;
-  font-size: 18px;
+
+.categories {
+  margin-top: 30px;
+  overflow: auto;
+  height: 100%;
 }
-a.logo {
-  font-size: 30px;
-  font-weight: 900;
-  color: white;
+
+.categories li {
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+  list-style: none;
+}
+
+.categories li:hover {
+  background: #755180;
+}
+
+.categories li.selected {
+  background: #755180;
+}
+
+.categories li a {
+  margin-right: 20px;
+  color: #eee3f6;
+  font-weight: 300;
+}
+.categories li a i {
+  color: #a392a6;
 }
 </style>
