1027ecfc6fd733a266c205deaf6753da69187300
padosum <makalu171@gmail.com>
2021-09-28 13:52:25 +0900

    🔨 update generate data
    - add link, tags

--- scripts/generate_index.ts
+++ scripts/generate_index.ts
@@ -1,43 +1,48 @@
-import { join, resolve } from 'path'
-import { writeFileSync, readdirSync, readFileSync } from 'fs'
-const YFM = require('yaml-front-matter')
+import { join, resolve } from 'path';
+import { writeFileSync, readdirSync, readFileSync } from 'fs';
 
-const dataPath = resolve('public', 'post_store')
-const postsPath = join(dataPath, 'docs')
+const YFM = require('yaml-front-matter');
+
+const dataPath = resolve('public', 'post_store');
+const postsPath = join(dataPath, 'docs');
 
 // Get categories
-const categoryFolders = readdirSync(postsPath)
+const categoryFolders = readdirSync(postsPath);
 
 // Create posts_index object
-const postsIndex = []
+const postsIndex = [];
 
 for (const categoryFolder of categoryFolders) {
   if (categoryFolder !== 'img') {
-    const categoryPath = join(postsPath, categoryFolder)
-    const postFiles = readdirSync(categoryPath)
+    const categoryPath = join(postsPath, categoryFolder);
+    const postFiles = readdirSync(categoryPath);
 
     for (const postFile of postFiles) {
-      const postPath = join(categoryPath, postFile)
-      const postContent = readFileSync(postPath).toString()
-      const raw = YFM.loadFront(postContent)
-      const [postFileId] = postFile.split('.md')
+      const postPath = join(categoryPath, postFile);
+      const postContent = readFileSync(postPath).toString();
+      const raw = YFM.loadFront(postContent);
+      const [postFileId] = postFile.split('.md');
 
-      delete raw.__content
-      const { title, publishDate, tags } = raw
+      delete raw.__content;
+      const { title, link, publishDate, tags } = raw;
+      const formatDate = publishDate.toISOString().split('T')[0];
 
       postsIndex.push({
         id: postFileId,
         section: categoryFolder,
         title,
-        publishDate,
+        link,
+        publishDate: formatDate,
         tags,
-        url: `post_store/posts/${categoryFolder}/${postFile}`,
-      })
+        url: `post_store/docs/${categoryFolder}/${postFile}`,
+      });
     }
   }
 }
 
-postsIndex.sort((a, b) => Date.parse(b.publishDate) - Date.parse(a.publishDate))
+postsIndex.sort(
+  (a, b) => Date.parse(b.publishDate) - Date.parse(a.publishDate),
+);
 
-const indexPath = join(dataPath, 'posts_index.json')
-writeFileSync(indexPath, JSON.stringify(postsIndex, null, 4))
+const indexPath = join(dataPath, 'posts_index.json');
+writeFileSync(indexPath, JSON.stringify(postsIndex, null, 4));
