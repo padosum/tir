6f6f75c903328d59fe904bfea6b940941632ea44
padosum <makalu171@gmail.com>
2021-09-29 22:57:00 +0900

    🔨 update template

--- create.rb
+++ create.rb
@@ -1,17 +1,21 @@
 require 'erb'
+require 'date'
+
 class Info
-  attr_accessor :title
+  attr_accessor :title, :today
+
   def template_binding
     binding
   end
 end
 
-category = ARGV[0];
-filename = ARGV[1];
-new_file = File.open("./docs/#{category}/#{filename}.md", "w+")
+category = ARGV[0]
+filename = ARGV[1]
+new_file = File.open("./public/post_store/docs/#{category}/#{filename}.md", "w+")
 template = File.read("./template.erb")
-rand_emoji = rand(('1F300'.hex)..('1F531'.hex)).chr('UTF-8');
-ifno = Info.new
-ifno.title = rand_emoji.concat(" " + ARGV[2]); 
-new_file << ERB.new(template).result(ifno.template_binding)
+# rand_emoji = rand(('1F300'.hex)..('1F531'.hex)).chr('UTF-8');
+frontmatter = Info.new
+frontmatter.title = ARGV[2]
+frontmatter.today = Date.today
+new_file << ERB.new(template).result(frontmatter.template_binding)
 new_file.close
