ddae974b94c9a2641ec68bfbe378238ab669bd84
padosum <makalu171@gmail.com>
2023-02-01 22:14:10 +0900

    📦 vite 설정 파일 추가
    - vue + ts + vite 관련 package 설치
    - index 파일은 root로 변경
    - eslint, prettier 설정 파일 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
   ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/multi-word-component-names": "off",
+  parserOptions: {
+    ecmaVersion: "latest",
   },
 };
