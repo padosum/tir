0bb4a15fac032c3f011c4a1bef8447beddd0564e
padosum <makalu171@gmail.com>
2023-02-01 22:18:18 +0900

    🚚 component 명 변경
    - 두음절로 수정, html 요소와구분을위해

--- src/components/UtterancesComment.vue
+++ src/components/UtterancesComment.vue
@@ -2,30 +2,33 @@
   <div ref="comment"></div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   props: {
-    repo: String,
+    repo: {
+      type: String,
+      required: true,
+    },
   },
   mounted() {
-    const utterances = document.createElement("script");
+    const utterances = document.createElement("script") as HTMLElement;
 
     let theme = "boxy-light";
     const savedTheme = localStorage.getItem("selected-theme");
     if (savedTheme !== null) {
       theme = savedTheme === "dark" ? "dark-blue" : "boxy-light";
     }
-    utterances.type = "text/javascript";
-    utterances.async = true;
-    utterances.crossorigin = "anonymous";
-    utterances.src = "https://utteranc.es/client.js";
 
+    utterances.setAttribute("type", "text/javascript");
+    utterances.setAttribute("async", "async");
+    utterances.setAttribute("crossorigin", "anonymous");
+    utterances.setAttribute("src", "https://utteranc.es/client.js");
     utterances.setAttribute("issue-term", "pathname");
     utterances.setAttribute("theme", theme);
     utterances.setAttribute("repo", this.repo);
 
     // script tag 삽입
-    this.$refs.comment.appendChild(utterances);
+    (this.$refs.comment as HTMLElement).appendChild(utterances);
   },
 };
 </script>
